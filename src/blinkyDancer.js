3eef8685da189b17d193d4d54fc4e8ca7a72b27e
Jacob Berg <coffeesnakes@gmail.com>
2020-12-01 18:04:07 -0800

    lead wins pokebattles, loser vanishes and lead returns to random position. Pokeball wins all battles by catching pokemon

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,5 +1,9 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
+
+  this.$node.removeClass('dancer');
+  this.$node.addClass('pokeball');
+  this.$node.append('<img src="./poki/pokeball.gif" />');
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
@@ -8,27 +12,5 @@ makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
-  this.$node.toggle();
+  // this.$node.toggle();
 };
-
-
-
-// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
-//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-//   // so we must keep a copy of the old version of this function
-
-//   var oldStep = blinkyDancer.step;
-
-//   blinkyDancer.step = function() {
-//     // call the old version of step at the beginning of any call to this new version of step
-//     oldStep();
-//     // toggle() is a jQuery method to show/hide the <span> tag.
-//     // See http://api.jquery.com/category/effects/ for this and
-//     // other effects you can use on a jQuery-wrapped html tag.
-//     blinkyDancer.$node.toggle();
-//   };
-
-//   return blinkyDancer;
-// };
--- src/init.js
+++ src/init.js
@@ -21,7 +21,6 @@ $(document).ready(function () {
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
-
     var dancer = new dancerMakerFunction(
       $('body').height() * Math.random(),
       $('body').width() * Math.random(),
@@ -44,7 +43,6 @@ $(document).ready(function () {
     var lead = window.dancers[(Math.floor(Math.random() * window.dancers.length))];
     var leadX = lead.left;
     var leadY = lead.top;
-
     var closest = null;
     var shortestDistance = null;
 
@@ -52,7 +50,7 @@ $(document).ready(function () {
       if (window.dancers[i] !== lead) {
         var ydif = (window.dancers[i].top - leadY);
         var xdif = (window.dancers[i].left - leadX);
-        var hypotenuse = Math.sqrt( (ydif * ydif) + (xdif * xdif) );
+        var hypotenuse = Math.sqrt((ydif * ydif) + (xdif * xdif));
 
         if (closest === null || hypotenuse < shortestDistance) {
           closest = window.dancers[i];
@@ -63,37 +61,77 @@ $(document).ready(function () {
 
     lead.$node.addClass('duo');
     closest.$node.addClass('duo');
+
+    var leadURL = '';
+    var closestURL = '';
+
+    console.log(lead.$node[0].className);
+    console.log(closest.$node[0].className);
+    if (lead.$node[0].className === 'pokeball duo') {
+      closestURL = 'url("poki/disappear.gif")';
+      leadURL = 'url("poki/pokeball.gif")';
+    } else if (closest.$node[0].className === 'pokeball duo') {
+      leadURL = 'url("poki/disappear.gif")';
+      closestURL = 'url("poki/pokeball.gif")';
+    } else {
+      if (lead.$node[0].className === 'charmander duo') {
+        leadURL = 'url("poki/char.gif")';
+      } else if (lead.$node[0].className === 'bulbasaur duo') {
+        leadURL = 'url("poki/bulbasaur.gif")';
+      } else if (lead.$node[0].className === 'squirtle duo') {
+        leadURL = 'url("poki/squirtle.gif")';
+      }
+
+      if (closest.$node[0].className === 'charmander duo') {
+        closestURL = 'url("poki/char.gif")';
+      } else if (closest.$node[0].className === 'bulbasaur duo') {
+        closestURL = 'url("poki/bulbasaur.gif")';
+      } else if (closest.$node[0].className === 'squirtle duo') {
+        closestURL = 'url("poki/squirtle.gif")';
+      }
+    }
+
+
     setTimeout(function () {
-      lead.$node.css({ 'content': 'url("poki/char.gif")' });
+      lead.$node.css({ 'content': leadURL });
     }, 1337);
     setTimeout(function () {
-      closest.$node.css({ 'content': 'url("poki/bulbasaur.gif")' });
+      closest.$node.css({ 'content': closestURL });
     }, 1337);
-    setTimeout(function () {
-      closest.$node.css({ 'content': 'url("poki/ripBulbasaur.gif")' });
-    }, 5040);
+
+
+    if ((lead.$node[0].className !== 'pokeball duo') && (closest.$node[0].className !== 'pokeball duo')) {
+      setTimeout(function () {
+        closest.$node.css({ 'content': 'url("poki/ripBulbasaur.gif")' });
+      }, 5040);
+    }
 
     // move lead and closest to center of stage
-    var centerPos1 = {top: 500, left: 500};
-    var centerPos2 = {top: 500, left: 575};
-    lead.$node.animate(centerPos1, 1000);
-    closest.$node.animate(centerPos2, 1000);
+    lead.$node.animate({ top: 575, left: 840 }, 1000);
+    closest.$node.animate({ top: 575, left: 950 }, 1000);
 
     // play fight song
     url = './audio/pokemon-battle.mp3';
     volume = 0.1;
     music(url, volume);
+
+    // move winner back to original position on dance floor and remove loser from page
+    var movePokemon = function() {
+      lead.$node.animate({ top: $('body').height() * Math.random(), left: $('body').width() * Math.random() }, 1000);
+      closest.$node.remove();
+    };
+    setTimeout(function() {
+      movePokemon();
+    }, 7000);
+
+    // remove loser from window.dancers
+    for (var i = 0; i < window.dancers.length; i++) {
+      if (window.dancers[i] === closest) {
+        window.dancers.splice(i, 1);
+      }
+    }
+
   });
-
-  // $('.lineup').on('click', function (e) {
-  //   for (let i = 0; i < window.dancers.length; ++i) {
-  //     window.dancers[i].lineUp();
-  //     url = './audio/pokemon.mp3';
-  //     volume = 0.005;
-  //     music(url, volume);
-  //   }
-  // });
-
 
 
   let music = function (url, vol) {
